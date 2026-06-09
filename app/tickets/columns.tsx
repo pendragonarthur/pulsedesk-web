d866855951bf4f051e1b50501b9bb0013c20a262
Arthur Quaresma <devarthurquarxma@outlook.com>
2026-06-09 14:29:38 -0300

    feat: add badge de status e prioridade

--- app/tickets/columns.tsx
+++ app/tickets/columns.tsx
@@ -4,6 +4,7 @@ import { ColumnDef } from "@tanstack/react-table"
 import { Ticket } from "@/types/ticket"
 import { TicketActions } from "../components/dashboard/ticket-actions"
 import { formatDate } from "@/helpers/format-date"
+import { Badge } from "@/components/ui/badge"
 
 interface ColumnsProps {
     onTicketUpdated: () => Promise<void>
@@ -24,10 +25,39 @@ export function getColumns({ onTicketUpdated, onTicketDeleted }: ColumnsProps): 
         {
             accessorKey: "status",
             header: "Status",
+            cell: ({ row }) => {
+                const status = row.getValue("status") as string
+                const statusVariants: Record<string, string> = {
+                    Aberto: "bg-green-800",
+                    EmAndamento: "bg-yellow-800",
+                    AguardandoRetorno: "bg-orange-800",
+                    Concluido: "bg-blue-800",
+                    Cancelado: "bg-gray-800"
+                }
+                const badgeClass = statusVariants[status] ?? "bg-gray-800"
+
+                return (
+                    <Badge className={badgeClass}>{status}</Badge>
+                )
+            }
         },
         {
             accessorKey: "priority",
             header: "Prioridade",
+            cell: ({ row }) => {
+                const priority = row.getValue("priority") as string
+                const priorityVariants: Record<string, string> = {
+                    Baixa: "bg-green-800",
+                    Media: "bg-yellow-800",
+                    Alta: "bg-orange-800",
+                    Urgente: "bg-red-800",
+                }
+                const badgeClass = priorityVariants[priority] ?? "bg-gray-800"
+
+                return (
+                    <Badge className={badgeClass}>{priority}</Badge>
+                )
+            }
         },
         {
             accessorKey: "updatedAt",
